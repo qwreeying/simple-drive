a847d66ce829485b05589335d666d24ba9e652b6
Qwreey <dev@qwreey.kr>
2023-11-23 00:25:37 +0900

    Add stylelint, tailwindcss, nextui, floating-ui

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+import nextui from "@nextui-org/react"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{js,ts,jsx,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	darkMode: "class",
+	plugins: [nextui()],
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import linaria from "@linaria/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	clearScreen: false,
-	plugins: [react()],
+	plugins: [react(), linaria()],
 	server: {
 		proxy: {
 			"/api": "http://localhost:8080",
